c61f79d5b4d9647c95b32db602279f0f306a6add
nghia_nun <nghiant@itsol.vn>
2022-07-30 09:53:48 +0700

    aa

--- src/main/java/io/github/tubean/myspringcrud/entity/User.java
+++ src/main/java/io/github/tubean/myspringcrud/entity/User.java
@@ -3,7 +3,7 @@ package io.github.tubean.myspringcrud.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "USER")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
